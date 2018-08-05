61aa0a97872d03cb73171a73fac367287a8f553e
Francheska <francheska-guzman@users.noreply.github.com>
2018-08-05 07:57:22 -0400

    Updating

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -9,9 +9,9 @@ class Footer extends Component {
     return (
       <div className='footer'>
         <h6>Handmade by <a href="https://github.com/francheska-guzman" target="_black">Francheska Guzman</a>.</h6>
-        <a href="https://github.com/francheska-guzman" target="_black"><img className="footer-icon" src={GitHub} alt="GitHub" /></a>
-        <a href="mailto:francheska-guzman@outlook.com"><img className="footer-icon" src={Email} alt="Email" /></a>
-        <a href="https://www.linkedin.com/in/francheska-guzman" target="_black"><img className="footer-icon" src={LinkedIn} alt="LinkedIn" /></a>
+          <a href="https://github.com/francheska-guzman" target="_black"><img className="footer-icon" src={GitHub} alt="GitHub" /></a>
+          <a href="mailto:francheska-guzman@outlook.com"><img className="footer-icon" src={Email} alt="Email" /></a>
+          <a href="https://www.linkedin.com/in/francheska-guzman" target="_black"><img className="footer-icon" src={LinkedIn} alt="LinkedIn" /></a>
       </div>
     );
   }
