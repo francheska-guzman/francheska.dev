a2281a1239c2cd164f6e787c97e66dba8f9da2c0
Francheska <francheska-guzman@users.noreply.github.com>
2018-08-05 17:55:42 -0400

    Updating

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import GitHub from "../assets/icons/github.png";
-import Email from "../assets/icons/email.png";
+import ResumeCV from "../assets/icons/cv.png";
 import LinkedIn from "../assets/icons/linkedin.png";
 
 class Footer extends Component {
@@ -10,7 +10,7 @@ class Footer extends Component {
       <div className='footer'>
         <h6>Handmade by <a href="https://github.com/francheska-guzman" target="_black">Francheska Guzman</a>.</h6>
           <a href="https://github.com/francheska-guzman" target="_black"><img className="footer-icon" src={GitHub} alt="GitHub" /></a>
-          <a href="mailto:francheska-guzman@outlook.com"><img className="footer-icon" src={Email} alt="Email" /></a>
+          <a href="https://francheska-guzman.github.io/resume/francheska-guzman-resume.pdf" target="_black"><img className="footer-icon" src={ResumeCV} alt="Resume/CV" /></a>
           <a href="https://www.linkedin.com/in/francheska-guzman" target="_black"><img className="footer-icon" src={LinkedIn} alt="LinkedIn" /></a>
       </div>
     );
