5674172f441286ea3bf87e137fb94d5acba07e53
Francheska <francheska-guzman@users.noreply.github.com>
2018-10-06 10:28:09 -0400

    Updating

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -9,11 +9,11 @@ class About extends Component {
         <h2>About</h2>
         <div className="flex">
 	        <img src={picture} alt="Francheska Guzman" />
-	        <p className="introduction">Francheska is a passionate web developer based in New York. Her primary role is working as a Jr. Front End Programmer at the <a href="https://www.nyfa.edu/" target="_black">New York Film Academy — School of Film and Acting</a>.</p>
+	        <p className="introduction">Francheska is a passionate web developer based in New York. She is currently working at the <a href="https://www.nyfa.edu/" target="_black">New York Film Academy</a>, and is responsible of coding and delivering E-mail Marketing Campaings, as well as maintain the website.</p>
 	      </div>
-        <p>Since 2014, she is an active member of <a href="https://www.utest.com/" target="_black">uTest</a>, a crowdsourced software testing community. Francheska has tested web and native applications by performing localization and exploratory testing, as well as executing test cases across modern platforms and devices. </p>
+        <p>Since 2014, she is a member of <a href="https://www.utest.com/" target="_black">uTest</a>, a crowdsourced software testing community. Francheska has tested web and native applications by performing localization and exploratory testing, as well as executing test cases across modern platforms and devices. </p>
         <p>Early 2018, Francheska joined <a href="http://www.secondlanguagedesign.com/" target="_black">Second Language Design</a>, a boutique NYC-based creative agency where she has contributed in building WordPress websites for clients such as <a href="https://painfreecats.org/" target="_black">The North Carolina State University’s College of Veterinary Medicine</a> and <a href="http://aprpc.com/" target="_black">Acupuncture Remedies</a>.</p>
-        <p>Her experience working with technologies such as HTML, CSS, SASS/SCSS, JavaScript, jQuery, React, Node.js, Express.js, PostgreSQL, API's, GitHub and the WordPress stack, makes her confident to work with both front-end and back-end development.</p>
+        <p>Her experience working with technologies such as HTML, CSS, SASS/SCSS, JavaScript, jQuery, PHP, React, Node.js, Express.js, PostgreSQL, API's, GitHub and the WordPress stack, makes her confident to work with both front-end and back-end development.</p>
       </div>
     );
   }
