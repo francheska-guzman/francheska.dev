c6add680ad860997894edda5277c05bc48e95241
francheska-guzman <11191798+francheska-guzman@users.noreply.github.com>
2019-01-06 13:21:19 -0500

    Updates

--- precache-manifest.c9792bf28f3bdcef80380ee8ad13f4de.js
+++ precache-manifest.c9792bf28f3bdcef80380ee8ad13f4de.js
@@ -8,8 +8,8 @@ self.__precacheManifest = [
     "url": "/static/css/main.b7c1a693.chunk.css"
   },
   {
-    "revision": "f97e3bbf73254b0112091d0192f17aec",
-    "url": "/static/media/slick.f97e3bbf.svg"
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
   },
   {
     "revision": "823238b4b7589ad5bf78",
@@ -64,16 +64,16 @@ self.__precacheManifest = [
     "url": "/static/media/cv.87f4e8b1.png"
   },
   {
-    "revision": "ced611daf7709cc778da928fec876475",
-    "url": "/static/media/slick.ced611da.eot"
-  },
-  {
     "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
     "url": "/static/media/slick.b7c9e1e4.woff"
   },
   {
     "revision": "d41f55a78e6f49a5512878df1737e58a",
     "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
   },
   {
     "revision": "823238b4b7589ad5bf78",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2cf43301745d35900a205416099570cc.js"
+  "/precache-manifest.c9792bf28f3bdcef80380ee8ad13f4de.js"
 );
 
 workbox.clientsClaim();
