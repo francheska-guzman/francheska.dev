69d2b133bca40de4fe17946031166d8ff197ea06
francheska-guzman <11191798+francheska-guzman@users.noreply.github.com>
2019-08-14 11:56:07 -0400

    Updating

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -9,7 +9,7 @@ class About extends Component {
         <h2>About</h2>
         <div className="flex">
 	        <img src={picture} alt="Francheska Guzman" />
-	        <p className="introduction">Francheska is a passionate web developer based in Puerto Rico. She worked for over a year coding and delivering E-mail Marketing Campaings, as well as provide website maintenance to the <a href="https://www.nyfa.edu/" target="_black">New York Film Academy</a>.</p>
+	        <p className="introduction">Francheska is a passionate web developer based in Puerto Rico. She worked for over a year coding and delivering E-mail Marketing Campaings, as well as provide website maintenance to the <a href="https://www.nyfa.edu/" target="_black">New York Film Academy (NYFA)</a>. Her current role is coding and testing the new NYFA's Online Applications.</p>
             <p>Early 2018 she joined <a href="http://www.secondlanguagedesign.com/" target="_black">Second Language Design</a>, a boutique NYC-based creative agency where she has contributed in building WordPress websites for clients such as <a href="https://painfreecats.org/" target="_black">The North Carolina State University’s College of Veterinary Medicine</a> and <a href="http://aprpc.com/" target="_black">Acupuncture Remedies</a>.</p>
             <p>Prior to her experience as a web developer, she was a member of <a href="https://www.utest.com/" target="_black">uTest</a>, a crowdsourced software testing community. Francheska has tested web and native applications by performing localization and exploratory testing, as well as executing test cases across modern platforms and devices. </p>
         </div>
