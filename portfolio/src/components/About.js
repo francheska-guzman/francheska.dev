316c2a1b35f237c1295d9f495b2f8b2a1c800456
Francheska <Francheska@Piratas-MacBook-Pro.local>
2022-05-07 09:19:45 -0400

    Updating About

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -9,7 +9,7 @@ class About extends Component {
         <h2>About</h2>
         <div className="flex">
 	        <img src={picture} alt="Francheska Guzman" />
-                <p className="introduction">Francheska has born and raised in San Juan, PR. She has 8+ years of combined experience in manual and automated software testing, quality assurance, and web development. During this time, she has worked on the Tech, Education, and Healthcare industries. Francheska is currently working as a Software Test Engineer II at <a href="https://abarcahealth.com">Abarca Health</a>, a Pharmacy Benefit Management (PBM) company.</p>
+                <p className="introduction">Francheska has born and raised in San Juan, PR. She has 8+ years of combined experience in manual and automated software testing, quality assurance, and web development. Francheska is currently working as a Software Test Engineer II at <a href="https://abarcahealth.com" target='_blank'>Abarca Health</a>, a Pharmacy Benefit Management (PBM) company.</p>
         </div>
       </div>
     );
