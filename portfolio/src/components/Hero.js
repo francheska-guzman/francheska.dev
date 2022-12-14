a0bf995ad30723db21bdb9355cf8e2dddc506a7c
Francheska <Francheska@Piratas-MacBook-Pro.local>
2022-12-13 21:23:21 -0400

    Updating

--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -5,7 +5,7 @@ class Hero extends Component {
     return (
       <div className='hero'>
         <h1>Francheska Guzmán</h1>
-        <h2 className="animation-fade-in"><a href="https://atsqa.org/certified-testers/profile/68f29f4f903049f5b415aa0280400854" target="_blank">ISTQB/ASTQB Certified Tester</a></h2>
+        <h2 className="animation-fade-in"><a href="https://atsqa.org/certified-testers/profile/68f29f4f903049f5b415aa0280400854" target="_blank">ISTQB Certified Tester</a></h2>
       </div>
     );
   }
